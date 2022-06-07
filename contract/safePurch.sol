27aa582760d963ed42a65370a6f114e4ed64b85c
Artem Ermakov <nawarius1993@gmail.com>
2022-06-08 00:50:58 +0400

    Done contract

---
+++ contract/safePurch.sol
@@ -0,0 +1,126 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+contract Purchase {
+    uint public value;
+    address payable public seller;
+    address payable public buyer;
+
+    enum State { Created, Locked, Release, Inactive }
+    // The state variable has a default value of the first member, `State.created`
+    State public state;
+    
+    uint public createdTime;
+    uint public purchaseTime;
+
+    modifier condition(bool condition_) {
+        require(condition_);
+        _;
+    }
+
+    /// Only the buyer can call this function.
+    error OnlyBuyer();
+    /// Only the seller can call this function.
+    error OnlySeller();
+    /// The function cannot be called at the current state.
+    error InvalidState();
+    /// The provided value has to be even.
+    error ValueNotEven();
+
+    modifier onlyBuyer() {
+        if (msg.sender != buyer)
+            revert OnlyBuyer();
+        _;
+    }
+
+    modifier onlySeller() {
+        if (msg.sender != seller)
+            revert OnlySeller();
+        _;
+    }
+
+    modifier inState(State state_) {
+        if (state != state_)
+            revert InvalidState();
+        _;
+    }
+
+    modifier buyerOrTime(uint _time) {
+        require(msg.sender == buyer || _time >= 5 * 60);
+        _;
+    }
+
+    event Aborted();
+    event PurchaseConfirmed();
+    event ItemReceived();
+    event SellerRefunded();
+
+    constructor() payable {
+        createdTime = block.timestamp;
+
+        seller = payable(msg.sender);
+        value = msg.value / 2;
+        if ((2 * value) != msg.value)
+            revert ValueNotEven();
+    }
+
+    function abort()
+        external
+        onlySeller
+        inState(State.Created)
+    {
+        emit Aborted();
+        state = State.Inactive;
+
+        seller.transfer(address(this).balance);
+    }
+
+    function confirmPurchase()
+        external
+        inState(State.Created)
+        condition(msg.value == (2 * value))
+        payable
+    {
+        emit PurchaseConfirmed();
+
+        buyer = payable(msg.sender);
+        state = State.Locked;
+
+        purchaseTime = block.timestamp;
+    }
+
+    function confirmReceived()
+        external
+        onlyBuyer
+        inState(State.Locked)
+    {
+        emit ItemReceived();
+        state = State.Release;
+
+        buyer.transfer(value);
+    }
+
+    function refundSeller()
+        external
+        onlySeller
+        inState(State.Release)
+    {
+        emit SellerRefunded();
+        state = State.Inactive;
+
+        seller.transfer(3 * value);
+    }
+
+    function completePurchase() 
+        external 
+        inState(State.Locked)
+        buyerOrTime(block.timestamp - purchaseTime)
+    {
+        emit ItemReceived();
+        state = State.Release;
+        buyer.transfer(value);
+
+        emit SellerRefunded();
+        state = State.Inactive;
+        seller.transfer(3 * value);
+    }
+}
